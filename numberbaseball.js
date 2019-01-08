39826089e2eaf68b2af7994361b30e737e882a37
Head <mrdoo309@gmail.com>
2019-01-08 11:49:52 +0900

    change getRanNum function

--- numberbaseball.js
+++ numberbaseball.js
@@ -3,82 +3,88 @@
 // 2018. 08. 22
 
 // get random number
-function getRanNum() {
-    const allNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+class NumberBaseballGame {
+  constructor() {
+    this.answer = this.getRanNum();
+  }
+
+  getRanNum() {
+    const allNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
     const answer = [];
     for (let i = 0; i < 3; i++) {
-        let ranNum = Math.floor(Math.random() * allNumbers.length);
-        answer[i] = allNumbers[ranNum];
-        allNumbers.splice(ranNum, 1);
+      let ranNum = Math.floor(Math.random() * allNumbers.length);
+      let oneNum = allNumbers.splice(ranNum, 1);
+      answer.push(oneNum[0]);
     }
-    saveAnswer = answer
-    return saveAnswer
+    return answer;
+	}
+	
 }
-getRanNum();
 
 // get user number
 function getUserNum() {
-    const arr = [];
-    const input = document.getElementById('input').value;
-    const userNum = Number(input)
-    arr[0] = Math.floor(userNum / 100)
-    arr[1] = Math.floor((userNum - (arr[0] * 100)) / 10)
-    arr[2] = Math.floor(userNum - (arr[0] * 100) - (arr[1] * 10))
-    return arr;
+  const userString = document.getElementById("input").value;
+  const userNum = userString.split("");
+  return userNum;
 }
 
 // print progress //divide
 function isCorrect(strikeNum, ballNum, progress) {
-    if (strikeNum === 3) {
-        progress.innerHTML = '스트라이크 아웃!'
-    } else if (strikeNum > 0 || ballNum > 0) {
-        output(strikeNum, ballNum, progress)   
-    } else {
-        progress.innerHTML = '포볼'
-    }
+  if (strikeNum === 3) {
+    progress.innerHTML = "스트라이크 아웃!";
+  } else if (strikeNum > 0 || ballNum > 0) {
+    output(strikeNum, ballNum, progress);
+  } else {
+    progress.innerHTML = "포볼";
+  }
 }
 
 function output(strikeNum, ballNum, progress) {
-    return progress.innerHTML = '스트라이크 : ' + strikeNum + '<br> 볼 : ' + ballNum
+  return (progress.innerHTML =
+    "스트라이크 : " + strikeNum + "<br> 볼 : " + ballNum);
 }
 
 // cheking strike
 function isStrike(ranNum, userNum) {
-    var strikeNum = 0;
-    for (var i = 0; i < userNum.length; i++) {
-        if (userNum[i] === ranNum[i]) {
-            strikeNum++
-        }
+  let strikeNum = 0;
+  for (let i = 0; i < userNum.length; i++) {
+    if (userNum[i] === ranNum[i]) {
+      strikeNum++;
     }
-    return strikeNum;
+  }
+  return strikeNum;
 }
 
 // checking ball
 function isBall(ranNum, userNum) {
-    var ballNum = 0;
-    for (var i = 0; i < userNum.length; i++) {
-        if (ranNum[i] === userNum[i + 1] || ranNum[i] === userNum[i + 2] || ranNum[i] === userNum[i - 1] || ranNum[i] === userNum[i -2]) {
-            ballNum++
-        }
+  let ballNum = 0;
+  for (let i = 0; i < userNum.length; i++) {
+    if (userNum[i] !== ranNum[i] && ranNum.includes(userNum[i])) {
+      ballNum++;
     }
-    return ballNum;
+  }
+  return ballNum;
 }
 
 function rule(nowNum) {
-    if (nowNum[0] === nowNum[1] || nowNum[1] === nowNum[2] || nowNum[2] === nowNum[0]) {
-        return alert('같은 숫자를 입력하지 마세요!');
-    } else if (nowNum.length != 3) {
-        return alert('3자리의 숫자만 입력해 주세요!');
-    } 
+  if (
+    nowNum[0] === nowNum[1] ||
+    nowNum[1] === nowNum[2] ||
+    nowNum[2] === nowNum[0]
+  ) {
+    return alert("같은 숫자를 입력하지 마세요!");
+  } else if (nowNum.length != 3) {
+    return alert("3자리의 숫자만 입력해 주세요!");
+  }
 }
 
 // play button
 function play() {
-    var progress = document.getElementById('progress');
-    var answer = saveAnswer
-    var nowNum = getUserNum();
-    rule(nowNum);
-    var strikeNum = isStrike(answer, nowNum);
-    var ballNum = isBall(answer, nowNum);
-    isCorrect(strikeNum, ballNum, progress);
+  var progress = document.getElementById("progress");
+  var answer = saveAnswer;
+  var nowNum = getUserNum();
+  rule(nowNum);
+  var strikeNum = isStrike(answer, nowNum);
+  var ballNum = isBall(answer, nowNum);
+  isCorrect(strikeNum, ballNum, progress);
 }
